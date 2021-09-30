35811bc82005ac61293533a9360ae18595265ecd
root <root@LAPTOP-02BT58J9.localdomain>
2021-09-30 21:10:57 +0700

    tesing

---
+++ tes/testing.cpp
@@ -0,0 +1,12 @@
+// Anggoro Yudha Pratama 
+// 20200801322 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int thn,hasil;
+    cout<<"Masukan tahun kelahiran anda = ";
+    cin>>thn;
+    hasil = 2021-thn;
+    cout<<"Berarti Usia kalian sekarang adalah "<<hasil<<" tahun "<<endl;
+}
