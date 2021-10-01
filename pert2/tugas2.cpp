c2e7a60eff436fb57ec934d87afc7dda7433e31f
root <root@LAPTOP-02BT58J9.localdomain>
2021-10-01 13:12:29 +0700

    tugas2

---
+++ pert2/tugas2.cpp
@@ -0,0 +1,15 @@
+// Anggoro Yudha Pratama
+// 20200801322 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int thn,hasil; 
+    cout<<"Masukan tahun kelahiran anda = ";
+    cin>>thn;
+    hasil = 2021 - thn;
+    cout<<"Berarti umur kalian sekarang adalah "<<hasil<<endl;
+    cin.get();
+    return 0;
+}
